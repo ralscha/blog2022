a352a4c42753dd4d6fa3298b4cd6cfecf6448a94
Ralph Schaer <ralphschaer@gmail.com>
2025-07-16 14:20:39 +0200

    Update V2__create_shedlock.sql

--- shedlockdemo/src/main/resources/db/migration/V2__create_shedlock.sql
+++ shedlockdemo/src/main/resources/db/migration/V2__create_shedlock.sql
@@ -1,5 +1,5 @@
 CREATE TABLE shedlock(
-	name VARCHAR(64) NOT NULL, 
+    name VARCHAR(64) NOT NULL, 
     lock_until TIMESTAMP NOT NULL,
     locked_at TIMESTAMP NOT NULL, 
     locked_by VARCHAR(255) NOT NULL, 
